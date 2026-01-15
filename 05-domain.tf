4eaec60921c812277775181e9b87b4e30ecb6f94
Andrew Siwko <asiwko@siwko.org>
2026-01-15 16:48:41 -0500

    public ip fix

--- 05-domain.tf
+++ 05-domain.tf
@@ -9,12 +9,12 @@ resource "linode_domain_record" "aws01_siwko_org" {
     domain_id = linode_domain.siwko_org.id
     name = "aws01"
     record_type = "A"
-    target = aws_instance.test_virtual_machine_01.*.public_ip
+    target = aws_instance.test_virtual_machine_01.public_ip
 }
 
 resource "linode_domain_record" "aws02_siwko_org" {
     domain_id = linode_domain.siwko_org.id
     name = "aws02"
     record_type = "A"
-    target = aws_instance.test_virtual_machine_02.*.public_ip
+    target = aws_instance.test_virtual_machine_02.public_ip
 }
