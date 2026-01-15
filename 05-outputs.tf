a7dfb2ba7957999ac7441a3e708d9c85fa1e71a5
Andrew Siwko <asiwko@siwko.org>
2026-01-15 15:42:51 -0500

    typos

--- 05-outputs.tf
+++ 05-outputs.tf
@@ -1,16 +1,16 @@
 # This allows us to pull information out of the Terraform state.
 # It is printed at the end of plan or apply but can be extracted with terraform output.
 output "asiwko-vm-id-01" {
-  value = aws_instance.test_virtual_machine-01.*
+  value = aws_instance.test_virtual_machine_01.*
 }
 output "asiwko-vm-id-02" {
-  value = aws_instance.test_virtual_machine-02.*
+  value = aws_instance.test_virtual_machine_02.*
 }
 
-output "asiwko-vm-public-ip-01" {
+output "asiwko-vm-public-ip_01" {
   value = aws_instance.test_virtual_machine-01.*.public_ip
 }
-output "asiwko-vm-public-ip-02" {
+output "asiwko-vm-public-ip_02" {
   value = aws_instance.test_virtual_machine-02.*.public_ip
 }
 
