0ea33e5a7396d3eb6403b40b2f1ffe009dc78e86
Andrew Siwko <asiwko@siwko.org>
2026-01-15 12:19:22 -0500

    back to custom vpc

--- 03-ec2.tf
+++ 03-ec2.tf
@@ -12,12 +12,13 @@ resource "aws_key_pair" "andrew_key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4TVQryVnLaUPDIcaV7iGRgQM0ImcmqWZmDs+oVRHS2+1kfz4ZSRlEOFa70ywhTyvTv+3Vxl8NhdsQHB8a6YEYMEChX0CtzRMRCn35qBMsEZd1BJQE8AdOniLr1z+g4KoAA0zS5g8e+PMK/MSA0Hj8xRRQB4zmhzrj22xsmVJAkznktbSIdM+Mdf+ATbFEIf+a5BAwuBuXXdn1IcpFqG5uH9nQUQ4+AuDxURLeIy+qfz6BhpqyxLThYJxRI5FtwAAaRQecKB8JEb+HRhGYtK58YoYFup0PSlFcyg+ppUPJWCSxIveK4nttLwDS74LPHH9CWJY+hxFEqHfY9+kxRY4knNJH9vGFWUVKqwg5qKz8WB8OgGj9b9O82NooEdLUVRWVr0IWDTrDMDbiEUtsufsY7X6xmh81hYzet4j3Ep2BvgnQXJ1/xV2C/zZ5LeL/OhtfS5eE2JhoG/CgHr4S3G4+jsegmjTLDCEwVybf5eFEsv9Qdj1uLgdotIp10yFvhn0= asiwk@DESKTOP-DADDY"
 }
 
-resource "aws_default_vpc" "default" {}
+# resource "aws_default_vpc" "default" {}
 
 resource "aws_security_group" "public_access" {
   name        = "allow_ssh_http_ping"
   description = "Allow Ping, SSH and HTTP inbound traffic"
-  vpc_id      = aws_default_vpc.default.id
+  vpc_id      = aws_vpc.custom_vpc.id
+  # vpc_id      = aws_default_vpc.default.id
 
   ingress {
     from_port   = 22
