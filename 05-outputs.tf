98918c4147af39e0c1fa61b137a57fa72ff14bdd
Andrew Siwko <asiwko@siwko.org>
2026-01-15 15:41:30 -0500

    fixed outputs

--- 05-outputs.tf
+++ 05-outputs.tf
@@ -1,10 +1,16 @@
 # This allows us to pull information out of the Terraform state.
 # It is printed at the end of plan or apply but can be extracted with terraform output.
-output "asiwko-vm-id" {
-  value = aws_instance.test_virtual_machine.*
+output "asiwko-vm-id-01" {
+  value = aws_instance.test_virtual_machine-01.*
+}
+output "asiwko-vm-id-02" {
+  value = aws_instance.test_virtual_machine-02.*
 }
 
-output "asiwko-vm-public-ip" {
-  value = aws_instance.test_virtual_machine.*.public_ip
+output "asiwko-vm-public-ip-01" {
+  value = aws_instance.test_virtual_machine-01.*.public_ip
+}
+output "asiwko-vm-public-ip-02" {
+  value = aws_instance.test_virtual_machine-02.*.public_ip
 }
 
