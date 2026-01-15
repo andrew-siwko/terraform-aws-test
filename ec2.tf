dd7e0b2f89ef6f56594a84b879a6fdfa0ac64657
Andrew Siwko <asiwko@siwko.org>
2026-01-14 23:17:42 -0500

    added key info

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,10 @@ data "aws_ami" "amazon_linux" {
     }
   }
 
-
+resource "aws_key_pair" "andrew_key" {
+  key_name   = "user-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC4TVQryVnLaUPDIcaV7iGRgQM0ImcmqWZmDs+oVRHS2+1kfz4ZSRlEOFa70ywhTyvTv+3Vxl8NhdsQHB8a6YEYMEChX0CtzRMRCn35qBMsEZd1BJQE8AdOniLr1z+g4KoAA0zS5g8e+PMK/MSA0Hj8xRRQB4zmhzrj22xsmVJAkznktbSIdM+Mdf+ATbFEIf+a5BAwuBuXXdn1IcpFqG5uH9nQUQ4+AuDxURLeIy+qfz6BhpqyxLThYJxRI5FtwAAaRQecKB8JEb+HRhGYtK58YoYFup0PSlFcyg+ppUPJWCSxIveK4nttLwDS74LPHH9CWJY+hxFEqHfY9+kxRY4knNJH9vGFWUVKqwg5qKz8WB8OgGj9b9O82NooEdLUVRWVr0IWDTrDMDbiEUtsufsY7X6xmh81hYzet4j3Ep2BvgnQXJ1/xV2C/zZ5LeL/OhtfS5eE2JhoG/CgHr4S3G4+jsegmjTLDCEwVybf5eFEsv9Qdj1uLgdotIp10yFvhn0= asiwk@DESKTOP-DADDY"
+}
 resource "aws_security_group" "public_access" {
   name        = "allow_ssh_http"
   description = "Allow SSH and HTTP inbound traffic"
@@ -44,6 +47,7 @@ resource "aws_security_group" "public_access" {
 resource "aws_instance" "test_virtual_machine" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
+  key_name = aws_key_pair.andrew_key.key_name
 
   tags = {
     Name = "asiwko-vm-01"
