dc62655bb9cbb705ead4efb7df5cba1c5f8383d9
Andrew Siwko <asiwko@siwko.org>
2026-01-15 21:05:34 -0500

    testing prexisting domain

--- 05-domain.tf
+++ 05-domain.tf
@@ -5,15 +5,35 @@ resource "linode_domain" "siwko_org" {
     soa_email = "asiwko@siwko.org"
 }
 
-resource "linode_domain_record" "aws01_siwko_org" {
-    domain_id = linode_domain.siwko_org.id
+resource "linode_domain" "siwko_test_org" {
+    type = "master"
+    domain = "siwko-test.org"
+    soa_email = "asiwko@siwko.org"
+}
+
+# resource "linode_domain_record" "aws01_siwko_org" {
+#     domain_id = linode_domain.siwko_org.id
+#     name = "aws01"
+#     record_type = "A"
+#     target = aws_instance.test_virtual_machine_01.public_ip
+# }
+
+# resource "linode_domain_record" "aws02_siwko_org" {
+#     domain_id = linode_domain.siwko_org.id
+#     name = "aws02"
+#     record_type = "A"
+#     target = aws_instance.test_virtual_machine_02.public_ip
+# }
+
+resource "linode_domain_record" "aws01_siwko_test_org" {
+    domain_id = linode_domain.siwko_test_org.id
     name = "aws01"
     record_type = "A"
     target = aws_instance.test_virtual_machine_01.public_ip
 }
 
-resource "linode_domain_record" "aws02_siwko_org" {
-    domain_id = linode_domain.siwko_org.id
+resource "linode_domain_record" "aws02_siwko_test_org" {
+    domain_id = linode_domain.siwko_test_org.id
     name = "aws02"
     record_type = "A"
     target = aws_instance.test_virtual_machine_02.public_ip
