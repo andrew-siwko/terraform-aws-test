cc57bcc83253a10b0a67da5782c64f10cf83c8a8
Andrew Siwko <asiwko@siwko.org>
2026-01-15 21:21:03 -0500

    setting back to siwko.org

--- 05-domain.tf
+++ 05-domain.tf
@@ -1,40 +1,21 @@
-# This will update the dns records in my siwko.org domain for the new instances.
-# resource "linode_domain" "siwko_org" {
-#     type = "master"
-#     domain = "testdomainfortf.org"
-#     soa_email = "asiwko@siwko.org"
-# }
-
-# resource "linode_domain_record" "aws01_siwko_org" {
-#     domain_id = linode_domain.siwko_org.id
-#     name = "aws01"
-#     record_type = "A"
-#     target = aws_instance.test_virtual_machine_01.public_ip
-# }
-
-# resource "linode_domain_record" "aws02_siwko_org" {
-#     domain_id = linode_domain.siwko_org.id
-#     name = "aws02"
-#     record_type = "A"
-#     target = aws_instance.test_virtual_machine_02.public_ip
-# }
-
-resource "linode_domain" "siwko_test_org" {
+This will update the dns records in my siwko.org domain for the new instances.
+resource "linode_domain" "siwko_org" {
     type = "master"
-    domain = "siwko-test.org"
+    domain = "siwko.org"
     soa_email = "asiwko@siwko.org"
 }
 
-resource "linode_domain_record" "aws01_siwko_test_org" {
-    domain_id = linode_domain.siwko_test_org.id
+resource "linode_domain_record" "aws01_siwko_org" {
+    domain_id = linode_domain.siwko_org.id
     name = "aws01"
     record_type = "A"
     target = aws_instance.test_virtual_machine_01.public_ip
 }
 
-resource "linode_domain_record" "aws02_siwko_test_org" {
-    domain_id = linode_domain.siwko_test_org.id
+resource "linode_domain_record" "aws02_siwko_org" {
+    domain_id = linode_domain.siwko_org.id
     name = "aws02"
     record_type = "A"
     target = aws_instance.test_virtual_machine_02.public_ip
 }
+
