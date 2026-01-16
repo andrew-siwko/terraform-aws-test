8c7957541de145710f6ee29cd75ff4896ce6db93
Andrew Siwko <asiwko@siwko.org>
2026-01-15 20:59:45 -0500

    test for overwrite

--- 05-domain.tf
+++ 05-domain.tf
@@ -1,7 +1,7 @@
 # This will update the dns records in my siwko.org domain for the new instances.
 resource "linode_domain" "siwko_org" {
     type = "master"
-    domain = "rvatroop466.org"
+    domain = "testdomainfortf.org"
     soa_email = "asiwko@siwko.org"
 }
 
