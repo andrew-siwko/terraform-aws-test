6e660e6c6c5c9c06aa0948e00e6ce9a69e8a3d2f
Andrew Siwko <asiwko@siwko.org>
2026-01-15 21:09:03 -0500

    testing

--- 05-domain.tf
+++ 05-domain.tf
@@ -1,15 +1,9 @@
 # This will update the dns records in my siwko.org domain for the new instances.
-resource "linode_domain" "siwko_org" {
-    type = "master"
-    domain = "testdomainfortf.org"
-    soa_email = "asiwko@siwko.org"
-}
-
-resource "linode_domain" "siwko_test_org" {
-    type = "master"
-    domain = "siwko-test.org"
-    soa_email = "asiwko@siwko.org"
-}
+# resource "linode_domain" "siwko_org" {
+#     type = "master"
+#     domain = "testdomainfortf.org"
+#     soa_email = "asiwko@siwko.org"
+# }
 
 # resource "linode_domain_record" "aws01_siwko_org" {
 #     domain_id = linode_domain.siwko_org.id
@@ -24,6 +18,12 @@ resource "linode_domain" "siwko_test_org" {
 #     record_type = "A"
 #     target = aws_instance.test_virtual_machine_02.public_ip
 # }
+
+resource "linode_domain" "siwko_test_org" {
+    type = "master"
+    domain = "siwko-test.org"
+    soa_email = "asiwko@siwko.org"
+}
 
 resource "linode_domain_record" "aws01_siwko_test_org" {
     domain_id = linode_domain.siwko_test_org.id
