085042c75fd2a7c160d3837c4844b271776404dd
Andrew Siwko <asiwko@siwko.org>
2026-01-17 23:10:47 -0500

    moved rhel images into the main ec2 file.

--- 04-ec2.tf
+++ 04-ec2.tf
@@ -52,3 +52,34 @@ resource "aws_instance" "test_virtual_machine_02" {
   vpc_security_group_ids = [aws_security_group.public_access.id]
   associate_public_ip_address = true
 }
+
+# Lets build two hard coded RHEL 9.7 images
+resource "aws_instance" "test_virtual_machine_03" {
+  ami           = "ami-0d40a6bf9d3bfc868"
+  instance_type = "m7i-flex.large"
+  key_name = aws_key_pair.ssh_user_key.key_name
+
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "asiwko-vm-03"
+  }
+  
+  vpc_security_group_ids = [aws_security_group.public_access.id]
+  associate_public_ip_address = true
+}
+
+resource "aws_instance" "test_virtual_machine_04" {
+  ami           = "ami-0d40a6bf9d3bfc868"
+  instance_type = "m7i-flex.large"
+  key_name = aws_key_pair.ssh_user_key.key_name
+
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "asiwko-vm-03"
+  }
+  
+  vpc_security_group_ids = [aws_security_group.public_access.id]
+  associate_public_ip_address = true
+}
