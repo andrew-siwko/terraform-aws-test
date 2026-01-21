dee6a1bfaa51d9a670b851e5a93405c18e1001c1
Andrew Siwko <asiwko@siwko.org>
2026-01-21 13:07:18 -0500

    and another missed update

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -1,5 +1,5 @@
 # This allows us to pull information out of the Terraform state.
 # It is printed at the end of plan or apply but can be extracted with terraform output.
 output "asiwko-vm-public-ip_01" {
-  value = aws_instance.test_virtual_machine_01.public_ip
+  value = aws_instance.virtual_machine_01.public_ip
 }
