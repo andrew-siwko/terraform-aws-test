5a44b1c6a3651f385811509cb5a8c6ab6c17aa82
Andrew Siwko <asiwko@siwko.org>
2026-01-28 14:40:14 -0500

    update end of port range

--- 03-security-group.tf
+++ 03-security-group.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "public_access" {
     
   ingress {
     from_port   = 8080
-    to_port     = 80
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
