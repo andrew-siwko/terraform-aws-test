6a493b85ae3e6fa7924e66eacbd385c4db7ec281
Andrew Siwko <asiwko@siwko.org>
2026-01-28 14:36:59 -0500

    open 8080 for tomcat

--- 03-security-group.tf
+++ 03-security-group.tf
@@ -18,6 +18,13 @@ resource "aws_security_group" "public_access" {
   }
     
   ingress {
+    from_port   = 8080
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
