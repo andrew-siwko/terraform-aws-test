c4cbdec3f68b6d55b09440aad7872218cae475cd
Andrew Siwko <asiwko@siwko.org>
2026-02-04 16:25:26 -0500

    Delete the private IP record; clean up comments

--- 05-domain.tf
+++ 05-domain.tf
@@ -1,5 +1,7 @@
-# This will update the dns records in my siwko.org domain for the new instances.
+# I use this once to get the siwko.org zone into the state file
+# terraform import linode_domain.siwko_org 1228113
 
+# This will update the dns records in my siwko.org domain for the new instances.
 resource "linode_domain" "siwko_org" {
     type = "master"
     domain = "siwko.org"
@@ -8,11 +10,6 @@ resource "linode_domain" "siwko_org" {
     retry_sec   = 30
     ttl_sec     = 30
 }
-# command to import the domain
-# Records for the public IP addresses.
-
-# use this once to get the zone into the state file
-# terraform import linode_domain.siwko_org 1228113
 
 # one record for the virtual machine
 resource "linode_domain_record" "aws01_siwko_org" {
@@ -22,13 +19,3 @@ resource "linode_domain_record" "aws01_siwko_org" {
     ttl_sec = 5
     target = aws_instance.virtual_machine_01.public_ip
 }
-
-# one record for the private address
-resource "linode_domain_record" "aws01p_siwko_org" {
-    domain_id = linode_domain.siwko_org.id
-    name = "aws01p"
-    record_type = "A"
-    ttl_sec = 5
-    target = aws_instance.virtual_machine_01.private_ip
-}
-
