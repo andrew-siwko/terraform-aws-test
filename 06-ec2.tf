48680cd84237a385541035a990358beed3b635dc
Andrew Siwko <asiwko@siwko.org>
2026-02-15 15:22:22 -0500

    remove old comments

--- 06-ec2.tf
+++ 06-ec2.tf
@@ -8,15 +8,8 @@ resource "aws_key_pair" "ssh_authorized_key" {
   public_key = file("/container_shared/ansible/ansible_rsa.pub")
 }
 
-# This block defines the EC2 instance.  The AMI refers to the image to use as selected above.
-# This was tested on a free account.  Only certain insnances are permitted on a free account.
-# I searched for these with the AWS cli and found t3.micro is allowed.
-# RHEL ran out of memory runnung ansible on this so I updated to m7i-flex.large
-# The key pulls in the key_pair above for ssh login.
-# The subnet connects the instance to the network we defined.  If we allow the default vpc
-# there is a danger that Terraform will try to delete the default VPC and fail after timing out.
+
 resource "aws_instance" "virtual_machine_01" {
-  # ami           = "ami-0d40a6bf9d3bfc868"
   ami           = local.target_ami_id
   instance_type = var.instance_type
 
