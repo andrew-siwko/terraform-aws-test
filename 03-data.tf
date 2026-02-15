a71a1e96aa0017ef1b728961abfea5d6eb4bbad3
Andrew Siwko <asiwko@siwko.org>
2026-02-14 20:59:14 -0500

    clean up code

--- 03-data.tf
+++ 03-data.tf
@@ -18,21 +18,6 @@ data "aws_ec2_instance_type" "details" {
   instance_type = each.value
 }
 
-# output "supported_types" {
-#   value = data.aws_ec2_instance_types.available.instance_types
-# }
-# output "filtered_lab_instances" {
-#   value = {
-#     for type, details in data.aws_ec2_instance_type.details : type => {
-#       mem_gb     = details.memory_size / 1024
-#       vcpus      = details.default_vcpus
-#     #   arch       = join(", ", details.supported_architectures)
-#     #   storage_gb = details.total_instance_storage
-#     }
-#     if details.memory_size <= 16384 && details.default_vcpus <= 4
-#   }
-# }
-
 output "filtered_lab_instances" {
   value = [
     for type, details in data.aws_ec2_instance_type.details:
@@ -40,16 +25,6 @@ output "filtered_lab_instances" {
     if details.memory_size <= 1024 && details.default_vcpus <= 2
   ]
 }
-
-# output "instance_catalog" {
-#   value = {
-#     for type, details in data.aws_ec2_instance_type.details : type => {
-#       arch = join(", ", details.supported_architectures)
-#       vcpus = details.default_vcpus
-#       mem_gb = details.memory_size / 1024
-#     }
-#   }
-# }
 
 data "aws_ami_ids" "redhat_ids" {
   owners      = ["309956199498"] # Official Red Hat Owner ID
@@ -73,11 +48,3 @@ output "redhat_images" {
     if strcontains(ami.name,"9.7")
   ])
 }
-# output "instance_types" {
-#   value = {
-#     for type in [data.aws_ec2_instance_types.available] : type.name => {
-#       id           = type.id
-#     #   architecture = ami.architecture
-#     }
-#   }
-# }
